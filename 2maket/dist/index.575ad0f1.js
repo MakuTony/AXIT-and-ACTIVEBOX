96032fa8a9b48c0833ca53878859754377cfe34a
MakuTony <tmakutonin@gmail.com>
2022-09-20 15:21:28 +0300

    message

---
+++ 2maket/dist/index.575ad0f1.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".header_burger").click(function(event) {
+        $(".header_burger,.header_menu").toggleClass("active");
+        $("body").toggleClass("lock");
+    });
+});
+
+//# sourceMappingURL=index.575ad0f1.js.map
